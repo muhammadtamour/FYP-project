2b8fc74eedd360a6bdc66594a08d351bf05931ab
Syed Rahat Murtaza <rahat_murtaza@outlook.com>
2020-06-11 20:39:30 +0500

    Firebase Initialized

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
-import SignIn from '../SignIn/index';
-import PasswordForget from '../PasswordForget/index.js';
 import * as ROUTES from '../../constants/routes';
+import SignIn from '../SignIn/index';
+import PasswordForget from '../PasswordForget/index';
+import Home from '../Home/index';
 
 const App = () => (
     <Router>
         <Route path={ROUTES.SIGN_IN} component={SignIn} />
         <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
+        <Route path={ROUTES.HOME} component={Home} />
     </Router>
 );
 
