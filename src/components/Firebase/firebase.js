a4d52275601c30e072f5d6b00ff4bcf6895122b6
Syed Rahat Murtaza <rahat_murtaza@outlook.com>
2020-06-12 08:46:47 +0500

    Loading Users Successfully

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -15,6 +16,7 @@ class Firebase {
         app.initializeApp(config);
 
         this.auth = app.auth();
+        this.firestore = app.firestore();
     }
 
     doSignInWithEmailAndPassword = (email, password) =>
@@ -23,6 +25,8 @@ class Firebase {
     doSignOut = () => this.auth.signOut();
 
     doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    users = () => this.firestore.collection("Users");
 }
 
 export default Firebase;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,168 +3,219 @@ import { withAuthorization, AuthUserContext } from '../Session';
 
 import { withFirebase } from '../Firebase';
 
-const Home = () => (
-    <AuthUserContext.Consumer>
-        {authUser => (
-            <div>
-                <div className="page-topbar ">
-                    <div className="logo-area">
-                    </div>
-                    <div className="quick-area">
-                        <div className="pull-left">
-                            <ul className="info-menu left-links list-inline list-unstyled">
-                                <li className="sidebar-toggle-wrap">
-                                    <a href="" data-toggle="sidebar" className="sidebar_toggle">
-                                        <i className="fa fa-bars" />
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="pull-right">
-                            <ul className="info-menu right-links list-inline list-unstyled">
-                                <li className="profile">
-                                    <a href="#" data-toggle="dropdown" className="toggle">
-                                        <img src="./assets/data/profile/profile.jpg" alt="user-image" className="img-circle img-inline" />
-                                        <span>Admin<i className /></span>
-                                    </a>
-                                    <ul className="dropdown-menu profile animated fadeIn">
-                                        <SignOutForm />
+class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: [],
+        };
+    }
+
+    componentDidMount() {
+        this.props.firebase.users().onSnapshot(snapshot => {
+            var tempUsers = [];
+
+            snapshot.docs.map(doc => {
+                tempUsers.push({
+                    id: doc.id,
+                    name: doc.get('name'),
+                    email: doc.get('email'),
+                    status: doc.get('status'),
+                });
+            })
+
+            this.setState({
+                users: tempUsers,
+            })
+
+            console.log(this.state.users);
+        });
+    }
+
+    render() {
+        return (
+            <AuthUserContext.Consumer>
+                {authUser => (
+                    <div>
+                        <div className="page-topbar ">
+                            <div className="logo-area">
+                            </div>
+                            <div className="quick-area">
+                                <div className="pull-left">
+                                    <ul className="info-menu left-links list-inline list-unstyled">
+                                        <li className="sidebar-toggle-wrap">
+                                            <a href="" data-toggle="sidebar" className="sidebar_toggle">
+                                                <i className="fa fa-bars" />
+                                            </a>
+                                        </li>
                                     </ul>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                {/* END TOPBAR */}
-                {/* START CONTAINER */}
-                <div className="page-container row-fluid container-fluid">
-                    {/* SIDEBAR - START */}
-                    <div className="page-sidebar fixedscroll">
-                        {/* MAIN MENU - START */}
-                        <div className="page-sidebar-wrapper" id="main-menu-wrapper" style={{ backgroundColor: '#16001f' }}>
-                            {/* USER INFO - START */}
-                            <div className="profile-info row">
-                                <div className="profile-image col-xs-4">
-                                    <a href="ui-profile.html">
-                                        <img alt="" src="./assets/data/profile/profile.jpg" className="img-responsive img-circle" />
-                                    </a>
                                 </div>
-                                <div className="profile-details col-xs-8">
-                                    <h3>
-                                        <a href="#">Admin</a>
-                                        {/* Available statuses: online, idle, busy, away and offline */}
-                                        <span className="profile-status online" />
-                                    </h3>
-                                    <p className="profile-title">admin@admin.com</p>
+                                <div className="pull-right">
+                                    <ul className="info-menu right-links list-inline list-unstyled">
+                                        <li className="profile">
+                                            <a href="#" data-toggle="dropdown" className="toggle">
+                                                <img src="./assets/data/profile/profile.jpg" alt="user-image" className="img-circle img-inline" />
+                                                <span>Admin</span>
+                                            </a>
+                                            <ul className="dropdown-menu profile animated fadeIn">
+                                                <SignOutForm users={this.state.users} />
+                                            </ul>
+                                        </li>
+                                    </ul>
                                 </div>
                             </div>
-                            {/* USER INFO - END */}
-                            <ul className="wraplist">
-                                <li className="menusection">Main</li>
-                                <li className="active">
-                                    <a href="index.html">
-                                        <i className="fa fa-dashboard" />
-                                        <span className="title">Dashboard</span>
-                                    </a>
-                                </li>
-                                <li className>
-                                    <a href="ui-calendar.html">
-                                        <i className="fa fa-user" />
-                                        <span className="title">User</span>
-                                    </a>
-                                </li>
-                                <li className="menusection">Posts</li>
-                                <li className>
-                                    <a href="ui-calendar.html">
-                                        <i className="fa fa-file-video-o" />
-                                        <span className="title">Post a Video</span>
-                                    </a>
-                                </li>
-                                <li className>
-                                    <a href="ui-calendar.html">
-                                        <i className="fa fa-leaf" />
-                                        <span className="title">Nutritional Plan</span>
-                                    </a>
-                                </li>
-                                <li className>
-                                    <a href="ui-calendar.html">
-                                        <i className="fa fa-calendar-o" />
-                                        <span className="title">Academy Schedule</span>
-                                    </a>
-                                </li>
-                                <li className>
-                                    <a href="ui-calendar.html">
-                                        <i className="fa fa-bell-o" />
-                                        <span className="title">Send Notification</span>
-                                    </a>
-                                </li>
-                            </ul>
                         </div>
-                        {/* MAIN MENU - END */}
-                    </div>
-                    {/*  SIDEBAR - END */}
-                    {/* START CONTENT */}
-                    <section id="main-content" className=" ">
-                        <section className="wrapper main-wrapper row" style={{}}>
-                            <div className="col-xs-12">
-                                <div className="page-title">
-                                    <div className="pull-left">
-                                        {/* PAGE HEADING TAG - START */}<h1 className="title">Dashboard</h1>{/* PAGE HEADING TAG - END */}                          </div>
-                                    <div className="pull-right hidden-xs">
-                                        <ol className="breadcrumb">
-                                            <li>
-                                                <a href="index.html"><i className="fa fa-home" />Home</a>
-                                            </li>
-                                            <li className="active">
-                                                <strong>Dashboard</strong>
-                                            </li>
-                                        </ol>
+                        {/* END TOPBAR */}
+                        {/* START CONTAINER */}
+                        <div className="page-container row-fluid container-fluid">
+                            {/* SIDEBAR - START */}
+                            <div className="page-sidebar fixedscroll">
+                                {/* MAIN MENU - START */}
+                                <div className="page-sidebar-wrapper" id="main-menu-wrapper" style={{ backgroundColor: '#16001f' }}>
+                                    {/* USER INFO - START */}
+                                    <div className="profile-info row">
+                                        <div className="profile-image col-xs-4">
+                                            <a href="ui-profile.html">
+                                                <img alt="" src="./assets/data/profile/profile.jpg" className="img-responsive img-circle" />
+                                            </a>
+                                        </div>
+                                        <div className="profile-details col-xs-8">
+                                            <h3>
+                                                <a href="#">Admin</a>
+                                                {/* Available statuses: online, idle, busy, away and offline */}
+                                                <span className="profile-status online" />
+                                            </h3>
+                                            <p className="profile-title">admin@admin.com</p>
+                                        </div>
                                     </div>
+                                    {/* USER INFO - END */}
+                                    <ul className="wraplist">
+                                        <li className="menusection">Main</li>
+                                        <li className="active">
+                                            <a href="index.html">
+                                                <i className="fa fa-dashboard" />
+                                                <span className="title">Dashboard</span>
+                                            </a>
+                                        </li>
+                                        <li className>
+                                            <a href="ui-calendar.html">
+                                                <i className="fa fa-user" />
+                                                <span className="title">Forms</span>
+                                            </a>
+                                        </li>
+                                    </ul>
                                 </div>
+                                {/* MAIN MENU - END */}
                             </div>
-                            <div className="clearfix" />
-                            {/* MAIN CONTENT AREA STARTS */}
-                            <div className="col-lg-12">
-                                <section className="box ">
-                                    <header className="panel_header">
-                                        <h2 className="title pull-left">New User Requests</h2>
-                                    </header>
-                                    <div className="content-body">    <div className="row">
-                                        <div className="col-xs-12">
-                                            <div className="table-responsive">
-                                                <table className="table table-bordered">
-                                                    <thead>
-                                                        <tr>
-                                                            <th>Name</th>
-                                                            <th>Email</th>
-                                                            <th>Action</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        <tr>
-                                                            <td>sohailahmad681</td>
-                                                            <td>sohailahmad68122@gmail.com</td>
-                                                            <td>
-                                                                <button type="button" className="btn btn-primary"><i className="fa fa-check" /></button>
-                                                                <button type="button" className="btn btn-primary"><i className="fa fa-close " /></button>
-                                                            </td>
-                                                        </tr>
-                                                    </tbody>
-                                                </table>
+                            {/*  SIDEBAR - END */}
+                            {/* START CONTENT */}
+                            <section id="main-content" className=" ">
+                                <section className="wrapper main-wrapper row" style={{}}>
+                                    <div className="col-xs-12">
+                                        <div className="page-title">
+                                            <div className="pull-left">
+                                                {/* PAGE HEADING TAG - START */}<h1 className="title">Dashboard</h1>{/* PAGE HEADING TAG - END */}                          </div>
+                                            <div className="pull-right hidden-xs">
+                                                <ol className="breadcrumb">
+                                                    <li>
+                                                        <a href="index.html"><i className="fa fa-home" />Home</a>
+                                                    </li>
+                                                    <li className="active">
+                                                        <strong>Dashboard</strong>
+                                                    </li>
+                                                </ol>
                                             </div>
                                         </div>
                                     </div>
-                                    </div>
-                                </section></div>
-                            {/* MAIN CONTENT AREA ENDS */}
-                        </section>
-                    </section>
-                    {/* END CONTENT */}
-                </div>
-            </div>
-        )}
-    </AuthUserContext.Consumer >
+                                    <div className="clearfix" />
+                                    {/* MAIN CONTENT AREA STARTS */}
+                                    <div className="col-lg-12">
+                                        <section className="box ">
+                                            <header className="panel_header">
+                                                <h2 className="title pull-left">New User Requests</h2>
+                                            </header>
+                                            <div className="content-body">    <div className="row">
+                                                <div className="col-xs-12">
+                                                    <div className="table-responsive">
+                                                        <table className="table table-bordered">
+                                                            <thead>
+                                                                <tr>
+                                                                    <th>Name</th>
+                                                                    <th>Email</th>
+                                                                    <th>Action</th>
+                                                                </tr>
+                                                            </thead>
+                                                            <UserList users={this.state.users} />
+                                                        </table>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            </div>
+                                        </section></div>
+                                    {/* MAIN CONTENT AREA ENDS */}
+                                </section>
+                            </section>
+                            {/* END CONTENT */}
+                        </div>
+                    </div>
+                )}
+            </AuthUserContext.Consumer >
+        );
+    }
+}
+
+const UserList = (props) => (
+    <tbody>
+        {
+            props.users.map(user => (
+                <tr>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>
+                        <UserListForm status={user.status} userId={user.id} />
+                    </td>
+                </tr>
+            ))
+        }
+    </tbody>
 );
+
+class UserListFormBase extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            status: props.status,
+        }
+    }
+
+    onClickReject = event => {
+        event.preventDefault();
+    }
+
+    onClickAccept = event => {
+        event.preventDefault();
+    }
+
+    render() {
+        const { status } = this.state;
+        if (status) {
+            return (
+                <button type="button" onClick={this.onClickReject} className="btn btn-primary"><i className="fa fa-close " /></button>
+            );
+        } else {
+            return (
+                <button type="button" onClick={this.onClickAccept} className="btn btn-primary"><i className="fa fa-check" /></button>
+            );
+        }
+    }
+}
+
+const UserListForm = withFirebase(UserListFormBase);
+
+export { UserListForm };
+
 
 class SignOutFormBase extends Component {
     constructor(props) {
